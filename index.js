3a4ae51801f622a5f92852e2037ad888fbd23af5
Stewart Dulaney <sdulaney@users.noreply.github.com>
2020-04-20 22:53:35 -0700

    Add comments

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond with one of the two variants returned from https://cfw-takehome.developers.workers.dev/api/variants.
+ * Each variant should be returned roughly 50% of the time.
  * @param {Request} request
  */
 async function handleRequest(request) {
